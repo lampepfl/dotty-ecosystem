83e1d179a611806e5d8d72fd974c829752434396
Anatolii <anatoliykmetyuk@gmail.com>
2020-01-22 11:33:33 +0100

    Add rudimentary completion

--- src/main/scala/ecosystem/impl/Repl.scala
+++ src/main/scala/ecosystem/impl/Repl.scala
@@ -4,13 +4,20 @@ package impl
 import scala.annotation.tailrec
 
 import org.jline.reader._
+import org.jline.reader.impl.completer._
 import ecosystem.model._
+import ecosystem.data.{ given Ecosystem }
 
 
 @main def Repl =
-  val reader = LineReaderBuilder.builder()
+  def reader = LineReaderBuilder.builder()
     .variable(LineReader.HISTORY_FILE, "history")
     .variable(LineReader.HISTORY_FILE_SIZE, 100)
+    .completer(ArgumentCompleter(
+      commandCompleter,
+      projectCompleter,
+      scalaVersionCompleter
+    ))
     .build()
 
   @tailrec def loop(): Unit =
@@ -32,3 +39,26 @@ def handle(line: String): Unit =
     catch
       case ParseException(msg) => return println(msg)
   executeCommand(cmd)
+
+def commandCompleter = StringsCompleter(
+  "show",
+  "show",
+  "exit",
+  "clone",
+  "clone",
+  "update",
+  "update",
+  "updateDotty",
+  "check",
+  "check",
+  "clean",
+  "compile",
+  "test",
+  "publishLocal",
+)
+
+def projectCompleter(given e: Ecosystem) =
+  StringsCompleter(e.all.map(_.name): _*)
+
+def scalaVersionCompleter = StringsCompleter(
+  data.dottyVersion, "2.13.1", "2.12.8")

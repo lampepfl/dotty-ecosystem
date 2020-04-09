1e42f732eeba732d7c9f1385e7b46d2a9da355ca
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-04-09 14:09:20 +0200

    Coloring for warnings and errors changed

--- src/main/scala/ecosystem/rendering/output.scala
+++ src/main/scala/ecosystem/rendering/output.scala
@@ -1,5 +1,5 @@
 package ecosystem.rendering
 
-def error(str: String) = println(s"${red("[error]")} $str")
-def warning(str: String) = println(s"${yellow("[warn]")} $str")
+def error(str: String) = println(s"[${red("error")}] $str")
+def warning(str: String) = println(s"[${yellow("warn")}] $str")
 def info(str: String) = println(s"[info] $str")

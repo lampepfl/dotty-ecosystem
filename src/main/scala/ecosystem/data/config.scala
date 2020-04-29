cb3e5e88c6b88e405e449f633d9393d22a57bd5c
Anatolii Kmetiuk <anatoliykmetyuk@gmail.com>
2020-04-29 12:08:14 +0200

    Update Dotty to 25th snapshot

--- src/main/scala/ecosystem/data/config.scala
+++ src/main/scala/ecosystem/data/config.scala
@@ -5,7 +5,7 @@ import better.files.File
 import ecosystem.model.{ given _, _ }
 
 val workdir = File("repos")
-val dottyVersion = "0.24.0-bin-SNAPSHOT"
+val dottyVersion = "0.25.0-bin-SNAPSHOT"
 
 lazy val sbtPluginFile =
   dotty.dir/"community-build/sbt-dotty-sbt"
